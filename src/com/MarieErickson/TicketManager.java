e42b5efd9a9deebe5eba14148c90ad5d34c0b36a
Marie8bit <yd7581ku@go.minneapolis.edu>
2016-10-16 21:13:49 -0500

    added user input options

--- src/com/MarieErickson/TicketManager.java
+++ src/com/MarieErickson/TicketManager.java
@@ -1,24 +1,62 @@
 package com.MarieErickson;
-
 import java.util.*;
+
 public class TicketManager {
 
     public static void main(String[] args) {
 
         LinkedList<Ticket> ticketQueue = new LinkedList<Ticket>();
 
+        Scanner scan = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("1. Enter Ticket\n2. Delete Ticket\n3. Display All Tickets\n4. Quit");
+            int task = Integer.parseInt(scan.nextLine());
+
+            if (task == 1) {
+                //Call addTickets, which will let us enter any number of new tickets
+                addTickets(ticketQueue);
+
+            } else if (task == 2) {
+                //delete a ticket
+                deleteTicket(ticketQueue);
+
+            } else if ( task == 4 ) {
+                //Quit. Future prototype may want to save all tickets to a file
+                System.out.println("Quitting program");
+                break;
+            }
+            else {
+                //this will happen for 3 or any other selection that is a valid int
+                //TODO Program crashes if you enter anything else - please fix
+                //Default will be print all tickets
+                printAllTickets(ticketQueue);
+            }
+        }
+
+        scan.close();
+
+    }
+
+    protected static void deleteTicket(LinkedList<Ticket> ticketQueue) {
+        //What to do here? Need to delete ticket, but how do we identify the ticket to delete?
+        //TODO implement this method
+        System.out.println("Delete tickets method called");
+    }
+
+    //Move the adding ticket code to a method
+    protected static void addTickets(LinkedList<Ticket> ticketQueue) {
         Scanner sc = new Scanner(System.in);
 
-        //Ask for some ticket info, create tickets, store in ticketQueue
-
+        boolean moreProblems = true;
         String description;
         String reporter;
-        //let's assume all tickets are created today, for testing. We can change this later.
+        //let's assume all tickets are created today, for testing. We can change this later if needed
         Date dateReported = new Date(); //Default constructor creates date with current date/time
         int priority;
 
-        boolean moreProblems = true;
-        while (moreProblems) {
+        while (moreProblems){
             System.out.println("Enter problem");
             description = sc.nextLine();
             System.out.println("Who reported this issue?");
@@ -29,26 +67,26 @@ public class TicketManager {
             Ticket t = new Ticket(description, priority, reporter, dateReported);
             ticketQueue.add(t);
 
-            //To test, let's print out all of the currently stored tickets after a new ticket is entered
+            //To test, let's print out all of the currently stored tickets
             printAllTickets(ticketQueue);
 
-            System.out.println("More tickets?");
+            System.out.println("More tickets to add?");
             String more = sc.nextLine();
             if (more.equalsIgnoreCase("N")) {
                 moreProblems = false;
             }
         }
 
-        sc.close();
-
     }
-
     protected static void printAllTickets(LinkedList<Ticket> tickets) {
-        System.out.println(" ------- All tickets ----------");
+        System.out.println(" ------- All open tickets ----------");
 
         for (Ticket t : tickets ) {
             System.out.println(t); //Write a toString method in Ticket class
-            //println will try to call toString on it's argument
+            //println will try to call toString on its argument
         }
+        System.out.println(" ------- End of ticket list ----------");
+
     }
 }
+
