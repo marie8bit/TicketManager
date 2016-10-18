66635369ac1ab5e829d2bffdab5b918e51e6435b
Marie8bit <yd7581ku@go.minneapolis.edu>
2016-10-17 19:43:42 -0500

    added delete and search methods

--- src/com/MarieErickson/Ticket.java
+++ src/com/MarieErickson/Ticket.java
@@ -8,26 +8,50 @@ import java.util.Date;
 public class Ticket {
 
     private int priority;
+
+
+
     private String reporter; //Stores person or department who reported issue
+
+
     private String description;
     private Date dateReported;
 
-    //We can autogenerate get and set methods if and when we need
-
-    //A constructor would be useful
+    //STATIC Counter - accessible to all Ticket objects.
+    //If any Ticket object modifies this counter, all Ticket objects will have the modified value
+    //Make it private - only Ticket objects should have access
+    private static int staticTicketIDCounter = 1;
+    //The ID for each ticket - instance variable. Each Ticket will have it's own ticketID variable
+    private int ticketID;
 
     public Ticket(String desc, int p, String rep, Date date) {
         this.description = desc;
         this.priority = p;
         this.reporter = rep;
         this.dateReported = date;
+        this.ticketID = staticTicketIDCounter;
+        staticTicketIDCounter++;
     }
 
-    //Called automatically if a Ticket object is an argument to System.out.println
-    public String toString(){
-        return(this.description + " Priority: " + this.priority + " Reported by: "
+    protected int getPriority() {
+        return priority;
+    }
+
+    public String toString() {
+        return ("ID= " + this.getTicketID() + " Issued: " + this.description
+                + " Priority: " + this.priority + " Reported by: "
                 + this.reporter + " Reported on: " + this.dateReported);
     }
 
+    public int getTicketID() {
+        return ticketID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public String getReporter() {
+        return reporter;
+    }
 }
 
--- src/com/MarieErickson/TicketManager.java
+++ src/com/MarieErickson/TicketManager.java
@@ -7,12 +7,14 @@ public class TicketManager {
 
         LinkedList<Ticket> ticketQueue = new LinkedList<Ticket>();
 
+
         Scanner scan = new Scanner(System.in);
 
         while(true){
 
-            System.out.println("1. Enter Ticket\n2. Delete Ticket\n3. Display All Tickets\n4. Quit");
-            int task = Integer.parseInt(scan.nextLine());
+            System.out.println("1. Enter Ticket\n2. Delete By ID \n3. Search by Name"+
+                    "\n4. Delete by Issue\n5. Display All Tickets\n6. Quit");
+            int task = getPositiveIntInput();
 
             if (task == 1) {
                 //Call addTickets, which will let us enter any number of new tickets
@@ -20,39 +22,142 @@ public class TicketManager {
 
             } else if (task == 2) {
                 //delete a ticket
-                deleteTicket(ticketQueue);
+                deleteTicketbyID(ticketQueue);
 
-            } else if ( task == 4 ) {
+            }  else if (task == 3) {
+                //search description for keyword and display list
+                searchTicketbyName(ticketQueue);
+
+            }  else if (task == 4) {
+                //delete a ticket
+                LinkedList<Ticket> n3 =searchTicketbyName(ticketQueue);
+                deleteTicketbyIssue(ticketQueue, n3);
+            }  else if (task == 5){
+                printAllTickets(ticketQueue);
+            }  else if (task == 6) {
                 //Quit. Future prototype may want to save all tickets to a file
                 System.out.println("Quitting program");
                 break;
+            } else {
+                //requests valid data from the user
+                System.out.println("Please choose an option from the list");
             }
-            else {
-                //this will happen for 3 or any other selection that is a valid int
-                //TODO Program crashes if you enter anything else - please fix
-                //Default will be print all tickets
-                printAllTickets(ticketQueue);
-            }
+
         }
 
         scan.close();
 
     }
 
-    protected static void deleteTicket(LinkedList<Ticket> ticketQueue) {
-        //What to do here? Need to delete ticket, but how do we identify the ticket to delete?
-        //TODO implement this method
-        System.out.println("Delete tickets method called");
+//    private static void searchTicketbyIssue(LinkedList<Ticket> ticketQueue) {
+//        LinkedList<Ticket> searchResults = new LinkedList<>();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter the keyword(s) you would like to search for");
+//        String search = scanner.nextLine();
+//        for (Ticket t :ticketQueue){
+//            if (t.getDescription().contains(search)){
+//                searchResults.add(t);
+//            }
+//            deleteTicketbyIssue( ticketQueue,searchResults);
+//            searchResults.clear();
+//        }
+//    }
+
+    private static LinkedList<Ticket> searchTicketbyName(LinkedList<Ticket> ticketQueue) {
+        LinkedList<Ticket> searchResults = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the keyword(s) you would like to search for");
+        String search = scanner.nextLine();
+        for (Ticket t :ticketQueue) {
+            if (t.getReporter().contains(search)) {
+                searchResults.add(t);
+            }
+        }
+        System.out.println("------List of tickets reported by"+search+"------");
+        printAllTickets(searchResults);
+        System.out.println("------End of List------");
+        return searchResults;
     }
+
+    protected static void deleteTicketbyID(LinkedList<Ticket> ticketQueue) {
+        printAllTickets(ticketQueue);   //display list for user
+
+        if (ticketQueue.size() == 0) {    //no tickets!
+            System.out.println("No tickets to delete!\n");
+            return;
+        }
+
+
+        System.out.println("Enter ID of ticket to delete");
+        int deleteID = getPositiveIntInput();
+
+        //Loop over all tickets. Delete the one with this ticket ID
+        boolean found = false;
+        for (Ticket ticket : ticketQueue) {
+            if (ticket.getTicketID() == deleteID) {
+                found = true;
+                ticketQueue.remove(ticket);
+                System.out.println(String.format("Ticket %d deleted", deleteID));
+                break; //don't need loop any more.
+            }
+        }
+
+        if (found == false) {
+            System.out.println("Ticket ID not found, no ticket deleted");
+            System.out.println("Would you like to try again? Y for yes");
+            Scanner sc = new Scanner(System.in);
+            String more = sc.nextLine();
+            if (more.equalsIgnoreCase("y")) {
+                deleteTicketbyID(ticketQueue);
+            }
+        }
+        printAllTickets(ticketQueue);  //print updated list
+
+    }
+
+    protected static void deleteTicketbyIssue(LinkedList<Ticket> ticketQueue,
+                                           LinkedList<Ticket> searchResults) {
+        printAllTickets(searchResults);   //display list for user
+
+        if (searchResults.size() == 0) {    //no tickets!
+            System.out.println("No tickets to delete!\n");
+            return;
+        }
+
+
+        System.out.println("Enter ID of ticket to delete");
+        int deleteID = getPositiveIntInput();
+
+        //Loop over all tickets. Delete the one with this ticket ID
+        boolean found = false;
+        for (Ticket ticket : searchResults) {
+            if (ticket.getTicketID() == deleteID) {
+                found = true;
+                ticketQueue.remove(ticket);
+                System.out.println(String.format("Ticket %d deleted", deleteID));
+                break; //don't need loop any more.
+            }
+        }
+
+        if (found == false) {
+            System.out.println("Ticket ID not found, no ticket deleted");
+            System.out.println("Would you like to try again? Y for yes");
+            Scanner sc = new Scanner(System.in);
+            String more = sc.nextLine();
+            if (more.equalsIgnoreCase("y")) {
+                deleteTicketbyIssue(ticketQueue, searchResults);
+            }
+        }
+        printAllTickets(ticketQueue);  //print updated list
+
+    }
+
 
     //Move the adding ticket code to a method
     protected static void addTickets(LinkedList<Ticket> ticketQueue) {
         Scanner sc = new Scanner(System.in);
-
         boolean moreProblems = true;
-        String description;
-        String reporter;
-        //let's assume all tickets are created today, for testing. We can change this later if needed
+        String description, reporter;
         Date dateReported = new Date(); //Default constructor creates date with current date/time
         int priority;
 
@@ -65,9 +170,9 @@ public class TicketManager {
             priority = Integer.parseInt(sc.nextLine());
 
             Ticket t = new Ticket(description, priority, reporter, dateReported);
-            ticketQueue.add(t);
+            //ticketQueue.add(t);
+            addTicketInPriorityOrder(ticketQueue, t);
 
-            //To test, let's print out all of the currently stored tickets
             printAllTickets(ticketQueue);
 
             System.out.println("More tickets to add?");
@@ -76,7 +181,34 @@ public class TicketManager {
                 moreProblems = false;
             }
         }
+    }
 
+    protected static void addTicketInPriorityOrder(LinkedList<Ticket> tickets, Ticket newTicket){
+
+        //Logic: assume the list is either empty or sorted
+
+        if (tickets.size() == 0 ) {//Special case - if list is empty, add ticket and return
+            tickets.add(newTicket);
+            return;
+        }
+
+        //Tickets with the HIGHEST priority number go at the front of the list. (e.g. 5=server on fire)
+        //Tickets with the LOWEST value of their priority number (so the lowest priority) go at the end
+
+        int newTicketPriority = newTicket.getPriority();
+
+        for (int x = 0; x < tickets.size() ; x++) {    //use a regular for loop so we know which element we are looking at
+
+            //if newTicket is higher or equal priority than the this element, add it in front of this one, and return
+            if (newTicketPriority >= tickets.get(x).getPriority()) {
+                tickets.add(x, newTicket);
+                return;
+            }
+        }
+
+        //Will only get here if the ticket is not added in the loop
+        //If that happens, it must be lower priority than all other tickets. So, add to the end.
+        tickets.addLast(newTicket);
     }
     protected static void printAllTickets(LinkedList<Ticket> tickets) {
         System.out.println(" ------- All open tickets ----------");
@@ -86,6 +218,28 @@ public class TicketManager {
             //println will try to call toString on its argument
         }
         System.out.println(" ------- End of ticket list ----------");
+
+    }
+    //Validation methods
+
+    private static int getPositiveIntInput() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                String stringInput = scanner.nextLine();
+                int intInput = Integer.parseInt(stringInput);
+                if (intInput >= 0) {
+                    return intInput;
+                } else {
+                    System.out.println("Please enter a vaild Ticket ID#");
+
+                    continue;
+                }
+            } catch (NumberFormatException ime) {
+                System.out.println("Please enter a valid Ticket ID#");
+            }
+
+        }
 
     }
 }
